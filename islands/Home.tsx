1706dad000c72b48582586b664db92e7a07d6d4b
Annin <143312793+Anninzy@users.noreply.github.com>
2024-02-22 08:29:05 +0800

    WIP parallax effect

--- islands/Home.tsx
+++ islands/Home.tsx
@@ -2,30 +2,19 @@ import { Head } from "$fresh/runtime.ts";
 import { signal } from "@preact/signals";
 import { useEffect } from "preact/hooks";
 
-const linkPositions = new Map();
+const linkCorrespondingPatternPosition = new Map();
 const currentPatternPosition = signal(0);
-let lastUpdatedSize = 0;
 type linkProps = {
   name: string;
 };
 
 function Link(props: linkProps) {
-  // START OF TODO
-  // figure out how to get the amount of links there are, and do positionOffset = 100 / amount_of_links, then for link 1, linkPositions.set(link1, positionOffset), link 2, linkPositions.set(link2, positionOffset * 2), link 3, linkPositions.set(link3, positionOffset * 3) and so on
-  if (!linkPositions.get(props.name)) {
-    linkPositions.set(props.name, 0);
-  }
-
-  if (linkPositions.size > lastUpdatedSize) {
-    lastUpdatedSize = linkPositions.size;
-    console.log(lastUpdatedSize);
-  } else {
-    console.log(linkPositions.size);
-  }
-  // END OF TODO
+  useEffect(() => {
+    linkCorrespondingPatternPosition.set(props.name, 0);
+  }, [])
 
   function shiftBackgroundAndPattern() {
-    currentPatternPosition.value = linkPositions.get(props.name);
+    currentPatternPosition.value = linkCorrespondingPatternPosition.get(props.name);
   }
 
   return (
@@ -39,7 +28,9 @@ function Link(props: linkProps) {
 }
 
 function EndOfLinks() {
-  console.log("test");
+  // for link 1, linkPositions.set(link1, positionOffset), link 2, linkPositions.set(link2, positionOffset * 2), and so on
+  const positionOffset = 100 / linkCorrespondingPatternPosition.size
+  return (<script />)
 }
 
 export default function () {
