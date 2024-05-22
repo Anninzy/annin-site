08bdb43e9fe45f79e86371e2639d487b2df93f29
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-22 01:10:05 +0000

    add lang attribute and remove default title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,11 @@
-import { type PageProps } from "$fresh/server.ts";
-export default function App({ Component }: PageProps) {
+import { PageProps } from "$fresh/server.ts";
+
+export default function ({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>annin-site</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
