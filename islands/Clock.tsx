d464e181d18a54109169b5f924c8781a01761240
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-22 02:27:42 +0000

    minor rewrite of clock

--- islands/Clock.tsx
+++ islands/Clock.tsx
@@ -1,7 +1,6 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { signal } from "@preact/signals";
 
-const time = signal("LO:AD:IN");
 const dateTimeFormat = new Intl.DateTimeFormat("en-US", {
   hour: "2-digit",
   minute: "2-digit",
@@ -10,12 +9,13 @@ const dateTimeFormat = new Intl.DateTimeFormat("en-US", {
   timeZone: "Etc/GMT-8",
 });
 
-function setClock() {
-  time.value = dateTimeFormat.format(new Date());
+function getClockString() {
+  return dateTimeFormat.format(new Date());
 }
 
+const time = signal(getClockString());
+
 export default function () {
-  setClock();
-  if (IS_BROWSER) setInterval(setClock, 1000);
+  if (IS_BROWSER) setInterval(time.value = getClockString(), 1000);
   return <span>{time}</span>;
 }
