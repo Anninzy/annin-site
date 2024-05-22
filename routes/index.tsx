ac0c868e23aff10ac840c935a8c1ab031f4076f8
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-22 01:42:41 +0000

    remove Clock island

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,31 +1,21 @@
 import { Head } from "$fresh/runtime.ts";
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+//import Clock from "../islands/Clock.tsx";
 
-export default function Home() {
-  const count = useSignal(3);
+export default function () {
   return (
     <>
       <Head>
-        <title>Home</title>
-        <meta name="description" content="Homepage of Annin's site."></meta>
+        <title>About</title>
+        <meta name="description" content="All about Annin."></meta>
       </Head>
 
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-          <p class="my-4">
-            Try updating this message in the
-            <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+      <div id="about" class="size-page position-center">
+        <div>
+          <h3>anninzy#0000</h3>
+          <h4>Professional procrastinator.</h4>
+          <p>
+            It is currently  for me.
           </p>
-          <Counter count={count} />
         </div>
       </div>
     </>
