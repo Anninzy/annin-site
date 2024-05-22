0122c2961766aab7a73e34f7616809b3606633c9
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-22 03:33:54 +0000

    autogen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,18 +2,18 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_HasNavbar_404 from "./routes/(HasNavbar)/_404.tsx";
 import * as $_HasNavbar_layout from "./routes/(HasNavbar)/_layout.tsx";
 import * as $_HasNavbar_index from "./routes/(HasNavbar)/index.tsx";
+import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $Clock from "./islands/Clock.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/(HasNavbar)/_404.tsx": $_HasNavbar_404,
     "./routes/(HasNavbar)/_layout.tsx": $_HasNavbar_layout,
     "./routes/(HasNavbar)/index.tsx": $_HasNavbar_index,
+    "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
   },
   islands: {
