6cee633abe20dc0cca2478b597f9dd295e5e1131
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 18:33:10 +0800

    format whitespace

--- islands/Clock.tsx
+++ islands/Clock.tsx
@@ -2,24 +2,24 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { signal } from "@preact/signals";
 
 const dateTimeFormat = new Intl.DateTimeFormat("en-US", {
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-  hour12: false,
-  timeZone: "Etc/GMT-8",
+	hour: "2-digit",
+	minute: "2-digit",
+	second: "2-digit",
+	hour12: false,
+	timeZone: "Etc/GMT-8",
 });
 
 function getClockString() {
-  return dateTimeFormat.format(new Date());
+	return dateTimeFormat.format(new Date());
 }
 
 function setTimeValue() {
-  time.value = getClockString();
+	time.value = getClockString();
 }
 
 const time = signal(getClockString());
 
 export default function () {
-  if (IS_BROWSER) setInterval(setTimeValue, 1000);
-  return <span>{time}</span>;
+	if (IS_BROWSER) setInterval(setTimeValue, 1000);
+	return <span>{time}</span>;
 }
