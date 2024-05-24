a04d458643708ebb009dd7c912d0317cec43c620
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 23:13:24 +0800

    revert cache on font as it is useless

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default function ({ Component }: PageProps) {
 				<meta charset="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="preconnect" href="https://fonts.bunny.net" />
-				<link href={asset("https://fonts.bunny.net/css?family=major-mono-display:400|space-mono:400,400i,700,700i&display=swap")} rel="stylesheet" />
+				<link href="https://fonts.bunny.net/css?family=major-mono-display:400|space-mono:400,400i,700,700i&display=swap" rel="stylesheet" />
 				<link rel="stylesheet" href={asset("/styles.css")} />
 			</head>
 
