89489f29bfae1b9ca1d1b6f356a019f3d863d4cb
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 17:15:44 +0800

    add placeholder pages

---
+++ routes/(HasNavbar)/blog.tsx
@@ -0,0 +1,7 @@
+export default function () {
+	return (
+		<div class="size-page position-child-center">
+			<h1>Coming soon as well.</h1>
+		</div>
+	);
+}
---
+++ routes/(HasNavbar)/portfolio.tsx
@@ -0,0 +1,7 @@
+export default function () {
+	return (
+		<div class="size-page position-child-center">
+			<h1>Coming soon too.</h1>
+		</div>
+	);
+}
