cc21d69ded50b10e2c4248a7496be48e1fbffe29
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 17:53:25 +0800

    add title

--- routes/(HasNavbar)/apps.tsx
+++ routes/(HasNavbar)/apps.tsx
@@ -1,7 +1,13 @@
 export default function () {
 	return (
-		<div class="size-page position-child-center">
-			<h1>Coming soon.</h1>
-		</div>
+		<>
+			<Head>
+				<title>Apps</title>
+			</Head>
+
+			<div class="size-page position-child-center">
+				<h1>Coming soon.</h1>
+			</div>
+		</>
 	);
 }
--- routes/(HasNavbar)/blog.tsx
+++ routes/(HasNavbar)/blog.tsx
@@ -1,7 +1,15 @@
+import { Head } from "$fresh/runtime.ts";
+
 export default function () {
 	return (
-		<div class="size-page position-child-center">
-			<h1>Coming soon as well.</h1>
-		</div>
+		<>
+			<Head>
+				<title>Blog</title>
+			</Head>
+
+			<div class="size-page position-child-center">
+				<h1>Coming soon as well.</h1>
+			</div>
+		</>
 	);
 }
