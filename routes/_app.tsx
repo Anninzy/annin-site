f02a1713d6ea5be17eca23c81db663154103e08d
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 23:09:23 +0800

    add caching to css and font

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { PageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts";
 
 export default function ({ Component }: PageProps) {
 	return (
@@ -7,8 +8,8 @@ export default function ({ Component }: PageProps) {
 				<meta charset="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="preconnect" href="https://fonts.bunny.net" />
-				<link href="https://fonts.bunny.net/css?family=major-mono-display:400|space-mono:400,400i,700,700i" rel="stylesheet" />
-				<link rel="stylesheet" href="/styles.css" />
+				<link href={asset("https://fonts.bunny.net/css?family=major-mono-display:400|space-mono:400,400i,700,700i&display=swap")} rel="stylesheet" />
+				<link rel="stylesheet" href={asset("/styles.css")} />
 			</head>
 
 			<body>
