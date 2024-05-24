ec2827a5297c12f2d7cfbc60b71b2638168f076b
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 17:15:03 +0800

    404 eye pulse

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,25 +1,26 @@
 import { Head } from "$fresh/runtime.ts";
 
 export default function () {
-  return (
-    <>
-      <Head>
-        <title>404</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>404</title>
+			</Head>
 
-      <div id="four-o-four" class="size-page position-child-center">
-        <div>
-          <div id="icon-inline-text">
-            <h1>&lt;4</h1>
-            <img src="/logo.svg" />
-            <h1>4&gt;</h1>
-          </div>
-          <h2>
-            When you stare into the abyss, the abyss beats you up for staring or
-            whatever.
-          </h2>
-        </div>
-      </div>
-    </>
-  );
+			<div id="four-o-four" class="size-page position-child-center">
+				<div>
+					<div id="icon-inline-text">
+						<h1>&lt;4</h1>
+						<a href="/">
+							<img src="/logo.svg" />
+						</a>
+						<h1>4&gt;</h1>
+					</div>
+					<h2>
+						When you stare into the abyss, the abyss beats you up for staring or whatever.
+					</h2>
+				</div>
+			</div>
+		</>
+	);
 }
