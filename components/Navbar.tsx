957a0c1692f9a053014a983c8b29fffda8a234e1
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-24 17:14:28 +0800

    format whitespace

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,22 @@
 export default function () {
-  return (
-    <ul id="navbar" class="position-child-vertical-center">
-      <li>
-        <a id="home-button" href="/">
-          <div class="position-child-center">
-            <img src="/logo.svg" />
-          </div>
-        </a>
-      </li>
-      <li>
-        <a href="/apps">Apps</a>
-      </li>
-      <li>
-        <a href="/blog">Blog</a>
-      </li>
-      <li>
-        <a href="/portfolio">Portfolio</a>
-      </li>
-    </ul>
-  );
+	return (
+		<ul id="navbar" class="position-child-vertical-center">
+			<li>
+				<a id="home-button" href="/">
+					<div class="position-child-center">
+						<img src="/logo.svg" />
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="/apps">Apps</a>
+			</li>
+			<li>
+				<a href="/blog">Blog</a>
+			</li>
+			<li>
+				<a href="/portfolio">Portfolio</a>
+			</li>
+		</ul>
+	);
 }
