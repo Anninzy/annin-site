25642ab2815f0ec4ab28e1355ee4074d79c71932
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-28 02:28:51 +0000

    remove font get from tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,11 +7,6 @@ export default function ({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.bunny.net" />
-        <link
-          href="https://fonts.bunny.net/css?family=major-mono-display:400|space-mono:400,400i,700,700i&display=swap"
-          rel="stylesheet"
-        />
         <link rel="stylesheet" href={asset("/styles.css")} />
       </head>
 
