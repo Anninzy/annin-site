e79f0af185484374dcf56b0d997510dbfa0cd81b
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-28 04:16:35 +0000

    add current page highlight

--- routes/(HasNavbar)/_layout.tsx
+++ routes/(HasNavbar)/_layout.tsx
@@ -4,7 +4,7 @@ export default function ({ Component }: PageProps) {
   return (
     <>
       <nav class="absolute w-screen h-12 bg-overlay0/50">
-        <ul class="font-major p-2 size-full flex flex-row items-center gap-6">
+        <ul id="navbar-list" class="font-major p-2 size-full flex flex-row items-center gap-6">
           <li class="h-full">
             <a class="size-full" href="/" aria-label="Go to home page">
               <div class="p-1 flex items-center justify-center h-full aspect-square bg-red rounded-full">
