369281e72ecd20c3c2507ab91837919fb89b23c3
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-28 02:24:58 +0000

    move navbar from individual tsx component into _layout.tsx

--- routes/(HasNavbar)/_layout.tsx
+++ routes/(HasNavbar)/_layout.tsx
@@ -1,10 +1,28 @@
 import { PageProps } from "$fresh/server.ts";
-import Navbar from "../../components/Navbar.tsx";
 
 export default function ({ Component }: PageProps) {
   return (
     <>
-      <Navbar />
+      <nav class="w-screen h-12 bg-overlay0/50">
+        <ul class="p-2 size-full flex flex-row items-center gap-6">
+          <li class="h-full">
+            <a class="size-full" href="/" aria-label="Go to home page">
+              <div class="p-1 flex items-center justify-center h-full aspect-square bg-red rounded-full">
+                <img class="size-full" src="/logo.svg" alt="" />
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/apps">Apps</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/portfolio">Portfolio</a>
+          </li>
+        </ul>
+      </nav>
       <Component />
     </>
   );
