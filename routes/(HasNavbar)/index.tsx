ed35b387e69bf9548e73bdba3887fd42c24d5532
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-28 03:46:32 +0000

    add navbar + content positioning/sizing in home page

--- routes/(HasNavbar)/index.tsx
+++ routes/(HasNavbar)/index.tsx
@@ -9,11 +9,15 @@ export default function () {
         <meta name="description" content="Oh hi."></meta>
       </Head>
 
-      <h1 class="">anninzy#0000</h1>
-      <h2>Professional procrastinator.</h2>
-      <p>
-        It is currently <Clock /> for me.
-      </p>
+      <div class="size-full flex items-center justify-center">
+        <div>
+          <h1 class="text-8xl">anninzy#0000</h1>
+          <h2 class="text-4xl">Professional procrastinator.</h2>
+          <p class="text-xl">
+            It is currently <Clock /> for me.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,14 +3,14 @@ import { asset } from "$fresh/runtime.ts";
 
 export default function ({ Component }: PageProps) {
   return (
-    <html lang="en" class="dark">
+    <html lang="en" class="dark size-full">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href={asset("/styles.css")} />
       </head>
 
-      <body class="latte bg-base dark:mocha dark:text-white">
+      <body class="latte bg-base dark:mocha dark:text-white size-full">
         <Component />
       </body>
     </html>
