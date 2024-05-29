7c2ac2cbd0c4ac7e8c7e1258cf774633d6cce84f
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-29 17:36:12 +0800

    remove script tag and redundant code

--- islands/SetTheme.tsx
+++ islands/SetTheme.tsx
@@ -1,13 +1,7 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-
 export default function () {
-  function checkTheme() {
-    console.log("test");
-    if (localStorage.getItem("useLightTheme")) {
-      document.documentElement.classList.remove("dark");
-    }
+  if (localStorage.getItem("useLightTheme")) {
+    document.documentElement.classList.remove("dark");
   }
-  checkTheme();
 
-  return <script onLoad={checkTheme} />;
+  return <></>;
 }
