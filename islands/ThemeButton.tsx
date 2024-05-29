01536271879340ccb9717c60be19879b0d19acfb
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-29 17:35:37 +0800

    remove debug console.log

--- islands/ThemeButton.tsx
+++ islands/ThemeButton.tsx
@@ -1,6 +1,5 @@
 export default function () {
   function changeTheme() {
-    console.log(localStorage.getItem("useLightTheme"));
     const rootClassList = document.documentElement.classList;
 
     if (localStorage.getItem("useLightTheme")) {
