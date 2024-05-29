f53e017e123fcf356513ea846a68fdf1d53ce708
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-29 06:28:32 +0000

    move functions inside the component

--- islands/Clock.tsx
+++ islands/Clock.tsx
@@ -1,5 +1,5 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
-import { signal } from "@preact/signals";
+import { useSignal } from "@preact/signals";
 
 const dateTimeFormat = new Intl.DateTimeFormat("en-US", {
   hour: "2-digit",
@@ -9,17 +9,17 @@ const dateTimeFormat = new Intl.DateTimeFormat("en-US", {
   timeZone: "Etc/GMT-8",
 });
 
-function getClockString() {
-  return dateTimeFormat.format(new Date());
-}
-
-function setTimeValue() {
-  time.value = getClockString();
-}
-
-const time = signal(getClockString());
-
 export default function () {
+  function getClockString() {
+    return dateTimeFormat.format(new Date());
+  }
+  
+  function setTimeValue() {
+    time.value = getClockString();
+  }
+  
+  const time = useSignal(getClockString());
   if (IS_BROWSER) setInterval(setTimeValue, 1000);
+
   return <span>{time}</span>;
 }
