bb068d375718608df55d623847fdeffc000fbc52
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-29 23:39:55 +0800

    add placeholder text

--- routes/(HasNavbar)/apps.tsx
+++ routes/(HasNavbar)/apps.tsx
@@ -7,6 +7,10 @@ export default function () {
         <title>Apps</title>
         <meta name="description" content="A collection of boring web apps." />
       </Head>
+
+      <p class="m-24">
+        What are some good web app ideas?
+      </p>
     </>
   );
 }
