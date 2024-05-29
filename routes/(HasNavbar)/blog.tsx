4e806e928764973fc530664e4f58570bd0c7eb17
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-29 23:36:54 +0800

    add placeholder blog

--- routes/(HasNavbar)/blog.tsx
+++ routes/(HasNavbar)/blog.tsx
@@ -7,6 +7,10 @@ export default function () {
         <title>Blog</title>
         <meta name="description" content="I write useless things." />
       </Head>
+
+      <p class="m-24">
+        Access blog #1 with <a href="/blog/1">this</a> link.
+      </p>
     </>
   );
 }
---
+++ routes/(HasNavbar)/blog/1.tsx
@@ -0,0 +1,3 @@
+export default function () {
+  return <p class="m-24">Oh hi. Welcome to blog #1. Empty for now.</p>;
+}
