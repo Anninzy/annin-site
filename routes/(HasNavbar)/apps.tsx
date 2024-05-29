d7682e3057c82df0700b60a29de4798e032c40aa
Annin <143312793+Anninzy@users.noreply.github.com>
2024-05-29 17:21:09 +0800

    remove old div

--- routes/(HasNavbar)/apps.tsx
+++ routes/(HasNavbar)/apps.tsx
@@ -8,10 +8,6 @@ export default function () {
         <meta name="description" content="A collection of boring web apps.">
         </meta>
       </Head>
-
-      <div class="size-page position-child-center">
-        <h1>Coming soon.</h1>
-      </div>
     </>
   );
 }
--- routes/(HasNavbar)/blog.tsx
+++ routes/(HasNavbar)/blog.tsx
@@ -7,10 +7,6 @@ export default function () {
         <title>Blog</title>
         <meta name="description" content="I write useless things."></meta>
       </Head>
-
-      <div class="size-page position-child-center">
-        <h1>Coming if I figure out what to write about.</h1>
-      </div>
     </>
   );
 }
