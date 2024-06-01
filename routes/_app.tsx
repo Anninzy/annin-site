1a1cfebe61adb9f7ce664d62c2b2823b7b5d22cd
Annin <143312793+Anninzy@users.noreply.github.com>
2024-06-01 21:16:02 +0800

    add favicon for chrome

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function ({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
         <link rel="stylesheet" href={asset("/styles.css")} />
       </head>
 
